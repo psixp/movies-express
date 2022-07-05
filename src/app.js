d6d338ca2ebcd82fd8ed0e13ff5344f09ca7d147
psixp <pablo_six@live.com>
2022-07-04 21:57:22 -0300

    subinido base de dados e rodando sequelize

--- src/app.js
+++ src/app.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const path = require('path');
+const {Sequelize} = require('sequelize');
 
 const indexRouter = require('./routes/index');
 
@@ -16,5 +17,22 @@ app.use(express.static(path.resolve(__dirname, '../public')));
 app.use('/', indexRouter);
 app.use(moviesRoutes);
 app.use(genresRoutes);
+
+const sequelize = new Sequelize('movies_db', 'root', '', {
+    host: 'localhost',
+    dialect: 'mysql',
+    port: '3306'
+});
+
+async function authenticate() {
+    try {
+        await sequelize.authenticate();
+        console.log('Servidor conectado com sucesso.');
+    } catch (error) {
+        console.error('Não foi possivel conectar por:', error);
+    }
+}
+authenticate()
+
 
 app.listen('3001', () => console.log('Servidor rodando na porta 3001'));
